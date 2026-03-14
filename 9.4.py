ebd1b4e61b6e2f2d933c0cfada897edb5f8c5bd7
uuuumy <e.idanova@g.nsu.ru>
2026-03-14 15:25:59 +0700

    Update 9.4.py

--- 9.4.py
+++ 9.4.py
@@ -1,5 +1,5 @@
-with open('input.txt', 'r', encoding='utf-8') as file:
-    with open('output.txt', 'w', encoding='utf-8') as new_file:
+with open('input.txt', 'r', encoding = 'utf-8') as file:
+    with open('output.txt', 'w', encoding = 'utf-8') as new_file:
         for line in file:
-            if len(line) > 20:
+            if len(line.strip()) > 20:
                 new_file.write(line)
